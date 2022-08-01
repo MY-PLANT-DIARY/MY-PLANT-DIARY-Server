ea2a5728f873f223f6d765eb20d5db6f0d1a7a68
이지민 <wlals060330@gmail.com>
2022-08-02 00:06:15 +0900

    grow domain

---
+++ src/main/java/com/myplantdiary/Grow/domain/entity/GrowLevel.java
@@ -0,0 +1,5 @@
+package com.myplantdiary.Grow.domain.entity;
+
+public enum GrowLevel {
+    LEVEL1 , LEVEL2, LEVEL3
+}
---
+++ src/main/java/com/myplantdiary/Grow/domain/entity/GrowStatus.java
@@ -0,0 +1,5 @@
+package com.myplantdiary.Grow.domain.entity;
+
+public enum GrowStatus {
+    USING, UNUSING
+}
