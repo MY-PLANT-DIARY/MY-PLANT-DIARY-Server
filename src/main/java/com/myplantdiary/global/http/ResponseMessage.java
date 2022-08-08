e87d3c289e5273151b51279c972d0a4ed0741e28
이지민 <wlals060330@gmail.com>
2022-08-08 20:36:16 +0900

    ADD: Http 규약 추ê가

---
+++ src/main/java/com/myplantdiary/global/http/ResponseMessage.java
@@ -0,0 +1,12 @@
+package com.myplantdiary.global.http;
+
+public class ResponseMessage {
+    public static final String LOGIN_SUCCESS = "로그인 성공";
+    public static final String LOGIN_FAIL = "로그인 실패";
+    public static final String CREATED_USER = "회원 가입 성공";
+    public static final String CREATED_POST = "게시물 등록 성공";
+    public static final String READ_POSTS = "게시물 정보 조회 성공";
+    public static final String READ_POST = "단일 게시물 정보 조회 성공";
+    public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
+    public static final String DB_ERROR = "데이터베이스 에러";
+}
