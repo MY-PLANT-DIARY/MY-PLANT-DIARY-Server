a92ea63fabf01d6a1c403bce7a393b552b7c3ed9
이지민 <wlals060330@gmail.com>
2022-08-09 14:56:37 +0900

    FIX: userId 로 조회 수정

--- src/main/java/com/myplantdiary/post/controller/PostController.java
+++ src/main/java/com/myplantdiary/post/controller/PostController.java
@@ -6,18 +6,11 @@ import com.myplantdiary.global.http.StatusCode;
 import com.myplantdiary.post.domain.entity.Post;
 import com.myplantdiary.post.dto.PostRequestDto;
 import com.myplantdiary.post.dto.PostResponseDto;
-import com.myplantdiary.post.dto.PostsResponseDto;
 import com.myplantdiary.post.service.PostService;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,11 +26,11 @@ public class PostController {
         return new ResponseEntity<>(DefaultRes.res(StatusCode.OK, ResponseMessage.CREATED_POST), HttpStatus.OK);
     }
 
-    @GetMapping("/posts")
-    public ResponseEntity posts(){
-        List<Post> posts = postService.findPosts();
-        List<PostsResponseDto> collect = posts.stream()
-                .map(post -> new PostsResponseDto(post.getId(), post.getImgUrl()))
+    @GetMapping("/posts/{userId}")
+    public ResponseEntity posts(@PathVariable("userId")Long userId){
+        List<Post> posts = postService.findPosts(userId);
+        List<PostResponseDto> collect = posts.stream()
+                .map(post -> new PostResponseDto(post.getPostDate(), post.getImgUrl(), post.getText()))
                 .collect(Collectors.toList());
         return new ResponseEntity<>(DefaultRes.res(StatusCode.OK, ResponseMessage.READ_POSTS,collect), HttpStatus.OK);
     }
--- src/main/java/com/myplantdiary/post/domain/repository/PostRepository.java
+++ src/main/java/com/myplantdiary/post/domain/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.myplantdiary.post.domain.repository;
 import com.myplantdiary.post.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByUserId(Long userId);
 }
